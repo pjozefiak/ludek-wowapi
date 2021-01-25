66b8f538fe204f3103a3455dc714423e42171fcf
pjozefiak <ptrjoze@gmail.com>
2021-01-25 01:58:16 +0100

    Encounter dumper small changes

--- tools/dumpers/dump_encounters.py
+++ tools/dumpers/dump_encounters.py
@@ -2,18 +2,18 @@ from WoWAPI import instances_encounters
 import yaml
 
 
-def dump_encounters():
-    with open('../../data/instances.yaml', 'r', encoding="utf-8") as expansions_file:
+def dump_encounters(region, token, namespace, locale):
+    with open('data/instances.yaml', 'r', encoding="utf-8") as expansions_file:
         instances_list = yaml.load(expansions_file, Loader=yaml.FullLoader)
 
     encounters_dict = {
-        'instances': {}
+        'dungeons': {},
+        'raids': {},
     }
 
     print(instances_list)
     for expansion in instances_list['instances']:
-        print(expansion)
         for dungeon in instances_list['instances'][expansion]['dungeons']:
-            print(dungeon)
-
-dump_encounters()
+            instance = instances_encounters.Encounters(region, token, namespace, locale, dungeon).get_encounters()
+            print(instance['name'])
+    print(encounters_dict)
